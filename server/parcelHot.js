685146b0069233dd27a8e4aa9fea14bcae5f0ee1
Jackson <Martinjackson@fda.gov>
2018-08-13 14:55:03 -0500

    smoother debugging

--- server/parcelHot.js
+++ server/parcelHot.js
@@ -1,20 +1,8 @@
 require("babel-core/register");
 require("babel-polyfill");
 
-const opn = require('opn');
 const Bundler = require('parcel-bundler');
 
-
-const openInBrowser = async (url, browser) => {
-  try {
-    const options = typeof browser === 'string' ? {app: browser} : undefined;
-
-    await opn(url, options);
-  } catch (err) {
-    console.error(`Unexpected error while opening in browser: ${browser}`);
-    console.error(err);
-  }
-}
 
 // How To Use:
 //    app.use(parseHot(port, '../src/index.js', {outDir: './public'}));
@@ -24,7 +12,7 @@ module.exports = function parcelHot(port, file='../src/index.js', options={outDi
   const bundler = new Bundler(file, options);
 
   bundler.once('buildEnd', ()=> {  
-    openInBrowser(`http://localhost:${port}`);  
+    console.log(`Ready for browser to view http://localhost:${port} -- vscode debugger "launch localhost" ...`);    
   });
 
   return bundler.middleware()
